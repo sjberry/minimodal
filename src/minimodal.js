ee31b51b8499d52a4de4b0746cd725c96ac57c36
Steven Berry <steven@sberry.me>
2014-11-25 01:35:23 -0800

    Lint fixes.

--- src/minimodal.js
+++ src/minimodal.js
@@ -2,7 +2,7 @@
  * @license
  * Copyright (C) 2013 Steven Berry (http://www.sberry.me/minimodal)
  * Licensed: MIT (http://opensource.org/licenses/mit-license.php)
- * 
+ *
  * Steven Berry
  * www.sberry.me
  * steven@sberry.me
@@ -19,25 +19,25 @@
 	else if (typeof root === 'object' && root && typeof root.document === 'object') {
 		factory.call(root, root.jQuery);
 	}
-})(this, function($, undefined) {
+})(this, function($) {
 	var window = this;
 	var document = window.document;
-	
+
 	var $shade;
-	
+
 	// Prepare the DOM with necessary elements for minimodal operation.
 	$(document).ready(function() {
 		// Create the minimodal shade element and insert it (hidden) into the document body.
 		$(document.body).append('<div id="minimodal-shade"></div>');
 		// Retain an internal reference to the shade element.
 		$shade = $('#minimodal-shade').hide();
-		
+
 		// Handle click events on any .modal-close classed element to close the active modal.
 		$(document).delegate('.modal-close', 'click', function(e) {
 			$.modal.close();
 		});
 	});
-	
+
 	/**
 	 * Helper function that constructs an object to be passed
 	 * into jQuery's .trigger() on `modalopen` and `modalclose`.
@@ -50,17 +50,17 @@
 	 */
 	function getEvent(type, obj) {
 		var add, event = {};
-		
+
 		if ($.isFunction(this.eventData)) {
 			$.extend(event, this.eventData.call(this.obj));
 		}
-		
+
 		$.extend(event, obj || {});
 		event.type = type;
-		
+
 		return event;
 	}
-	
+
 	/**
 	 * An internally referenced object that keeps track of key variables
 	 * and associates a $.fn.modal() call with some prototype functions.
@@ -86,7 +86,7 @@
 		// This will prevent option parameters from overwriting
 		// the prototype `open` and `close`
 		$.extend(this, options);
-		
+
 		return this;
 	}
 	Modal.prototype = {
@@ -101,13 +101,13 @@
 			if ($.isFunction(this.preOpen)) {
 				this.preOpen.apply(this.obj, arguments);
 			}
-			
+
 			// Move the modal object to the document body (instead of wherever it was).
 			// This helps with the stacking order to make sure that absolute/fixed position
 			// elements don't pop up over the modal.
 			$(document.body).append(this.obj);
 			this.obj.after($shade);
-			
+
 			// Display the shade DOM object.
 			$shade.show();
 			// Display and center the specified modal.
@@ -118,29 +118,29 @@
 					marginLeft: -this.obj.outerWidth() / 2
 				})
 				.trigger(getEvent.call(this, 'modalopen'));
-			
+
 			// Focus the first available input in the opened modal.
 			this.obj.find('input, select, textarea, button').first().focus();
-			
+
 			// Apply the postOpen function if it's specified.
 			if ($.isFunction(this.postOpen)) {
 				this.postOpen.apply(this.obj, arguments);
 			}
-			
+
 			// Create a keydown listener for the ESC key to close the active modal.
 			$(document).on('keydown.modal', function(e) {
 				// Do I really want this? It's necessary in one of my applications...
 				// But I don't know if it's just because I didn't design the events/DOM properly.
 				e.stopPropagation();
-				
+
 				if (e.which == 27) { // ESC key closes the modal window.
 					$.modal.close();
 				}
 			});
-			
+
 			return this;
 		},
-		
+
 		/**
 		 * Performs modal pre-processing, closes the active modal DOM element,
 		 * and finally performs modal post-processing.
@@ -152,13 +152,13 @@
 			if ($.isFunction(this.preClose)) {
 				this.preClose.apply(this.obj, arguments);
 			}
-			
+
 			// Hide the shade DOM object.
 			$shade.hide();
 			// Hide the active modal and fire the `modalclose` event with
 			// parameters obtained from the `eventData` option.
 			this.obj.hide().trigger(getEvent.call(this, 'modalclose', obj));
-			
+
 			// Reposition the DOM element back under its original parent.
 			this.parent.append(this.obj);
 			// Focus the pre-modal focused element.
@@ -166,19 +166,19 @@
 			$(this.focused).focus();
 			// Clear the modal reference from the jQuery object.
 			$.modal = null;
-			
+
 			// Apply the postClose function if it's specified.
 			if ($.isFunction(this.postClose)) {
 				this.postClose.apply(this.obj, arguments);
 			}
-			
+
 			// Clear out the keydown listener for the ESC key.
 			$(document).off('keydown.modal');
-			
+
 			return this;
 		}
 	};
-	
+
 	$.fn.extend({
 		/**
 		 * jQuery fn extension function to expose the internal `Modal` class and functionality.
@@ -193,14 +193,14 @@
 		 */
 		modal: function(options) {
 			options = options || {};
-			
+
 			// Check to see if there's actually an object.
 			if (this.length >= 1 && this[0].nodeType) {
 				// Close all existing modals that were created with the minimodal plugin.
 				if ($.modal instanceof Modal) {
 					$.modal.close();
 				}
-				
+
 				// Set the active modal and return it.
 				return $.modal = new Modal(this.first(), options).open();
 			}
